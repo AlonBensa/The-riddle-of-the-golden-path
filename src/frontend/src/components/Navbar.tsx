83d9ac2d35621886504c98288d3dc5a5e4099ed9
alonb <alon.b@mynk.ai>
2024-07-22 00:28:07 +0300

    Add navbar with coordinates input and planes amount input

---
+++ src/frontend/src/components/Navbar.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import { Typography, Button, Box } from '@mui/material';
+import { NavbarOptions } from '../pages/Home/Home';
+
+interface NavbarProps {
+  onNavbarOptionSelected: (option: NavbarOptions) => void;
+}
+
+export default function Navbar(props: NavbarProps) {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            MyApp
+          </Typography>
+          <Button 
+            color="inherit"
+            sx={{mr: 6}}
+            onClick={() => props.onNavbarOptionSelected(NavbarOptions.ChangePlanesAmount)}
+          >
+            Change planes amount
+          </Button>
+          <Button
+            color="inherit"
+            sx={{mr: 6}}
+            onClick={() => props.onNavbarOptionSelected(NavbarOptions.AddCoordinates)}
+          >
+            Add coordinates
+          </Button>
+          <Button
+            color="inherit"
+            sx={{mr: 2}}
+            onClick={() => props.onNavbarOptionSelected(NavbarOptions.RetrievePastOperations)}
+          >
+            Retrieve Past Operations
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
